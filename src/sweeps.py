2d252bb9284596399c769e04c962d5a63ef6c665
Lablack Mourad <mouradost@gmail.com>
2023-06-19 13:25:42 +0800

    Update sweeps.py

--- src/sweeps.py
+++ src/sweeps.py
@@ -22,6 +22,12 @@ from helpers.utils import Scaler
     config_name="config",
 )
 def main(cfg: DictConfig) -> None:
+    # Initiate logger
+    init_plot(cfg.log.style)
+    cfg.log.level = logging.getLevelName(cfg.log.level)
+    cfg.log.global_level = logging.getLevelName(cfg.log.global_level)
+    logger = logging.getLogger("Sweeps")
+    logger.info("Sweeps logs will be saved at %s", Path.cwd())
     # Choosing a device
     if cfg.device == "auto":
         cfg.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -30,9 +36,20 @@ def main(cfg: DictConfig) -> None:
     train_dataset = instantiate(cfg.dataset, mode="train")
     val_dataset = instantiate(cfg.dataset, mode="val")
     # Initiate the scaler
-    scaler = Scaler(*train_dataset.get_scaler_info())
+    scaler = Scaler(*train_dataset.get_scaler_info)
     # Initiate the model
-    model = instantiate(cfg.model, degrees=train_dataset.degree)
+    model = instantiate(cfg.model, degrees=train_dataset.degrees)
+    # Initiate the estimator model
+    e_model = instantiate(
+        cfg.estimator,
+        embedding_dict={
+            "time": 24 * 7 * 12,
+            "day": 7,
+            "node": train_dataset.node,
+            "degree": train_dataset.degrees_max,
+        },
+        degrees=train_dataset.degrees,
+    )
     # Setup a logger
     wandb.init(
         project=cfg.log.project,
@@ -40,7 +57,7 @@ def main(cfg: DictConfig) -> None:
     )
     # Initiate trainer
     trainer = instantiate(
-        cfg.trainer, model=model, scaler=scaler, logger_name=cfg.log.logger
+        cfg.trainer, model=model, e_model=e_model, scaler=scaler
     )
     # Train
     trainer.train(train_dataset.get_data_loader(), val_dataset.get_data_loader())
